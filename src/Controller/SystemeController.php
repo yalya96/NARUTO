aebdfc5385b3180229ac0dcb040644aa0a032376
yalya96 <yalya96@gmail.com>
2019-08-14 18:47:00 +0000

    Ajout Profil

--- src/Controller/SystemeController.php
+++ src/Controller/SystemeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Profil;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SystemeController extends AbstractController
@@ -15,5 +19,35 @@ class SystemeController extends AbstractController
         return $this->render('systeme/index.html.twig', [
             'controller_name' => 'SystemeController',
         ]);
+    }
+    /**
+     * @Route("/profil")
+     */
+    public function addprofil(Request $request, EntityManagerInterface $entityManagerInterface)
+    {
+        $profil = new Profil();
+        $form = $this->createForm(ProfilType::class, $profil);
+        $form->handleRequest($request);
+        $reception=$request->request->all();
+        $form->submit($reception);
+        $nom=$reception['libeller'];
+        $nom=strtoupper($nom);
+        $nom='["ROLE_'.$nom.'"]';
+        $profil->setLibeller($nom);
+
+            $entityManagerInterface->persist($profil);
+            $entityManagerInterface->flush();
+            $data = [
+                'status' => 201,
+                'message' => 'merci'
+            ];
+            return new JsonResponse($data, 201);
+    }
+    /**
+     * @Route("/ajoutsysteme")
+     */
+    public function addsys(Request $request, EntityManagerInterface $entityManagerInterface)
+    {
+        
     }
 }
